d4271b7b591a406555c2531de23b35cf867950fd
Junichi Kato <j5ik2o@gmail.com>
2023-08-31 20:46:51 +0900

    add files

---
+++ src/key_resolver.rs
@@ -0,0 +1,25 @@
+use std::collections::hash_map::DefaultHasher;
+use std::fmt::Debug;
+use std::hash::{Hash, Hasher};
+
+pub trait KeyResolver: Debug + Send + 'static {
+  fn resolve_pkey(&self, id_type_name: &str, value: &str, shard_count: u64) -> String;
+  fn resolve_skey(&self, id_type_name: &str, value: &str, seq_nr: usize) -> String;
+}
+
+#[derive(Debug, Clone)]
+pub struct DefaultPartitionKeyResolver;
+
+impl KeyResolver for DefaultPartitionKeyResolver {
+  fn resolve_pkey(&self, id_type_name: &str, value: &str, shard_count: u64) -> String {
+    let mut hasher = DefaultHasher::new();
+    value.hash(&mut hasher);
+    let hash_value = hasher.finish();
+    let remainder = hash_value % shard_count;
+    format!("{}-{}", id_type_name, remainder)
+  }
+
+  fn resolve_skey(&self, id_type_name: &str, value: &str, seq_nr: usize) -> String {
+    format!("{}-{}-{}", id_type_name, value, seq_nr)
+  }
+}
---
+++ src/types.rs
@@ -0,0 +1,58 @@
+use anyhow::Result;
+use async_trait::async_trait;
+use chrono::{DateTime, Utc};
+use serde::{de, Serialize};
+use std::fmt::Debug;
+
+/// 集約のIDを表すトレイト。
+pub trait AggregateId: std::fmt::Display + Debug + Send + Sync {
+  /// 集約の種別名を返す。
+  fn type_name(&self) -> String;
+  /// 集約のIDを文字列として返す
+  fn value(&self) -> String;
+}
+
+/// イベントを表すトレイト。
+pub trait Event: Debug + Send + Sync {
+  type ID: std::fmt::Display;
+  type AggregateID: AggregateId;
+  fn id(&self) -> &Self::ID;
+  fn aggregate_id(&self) -> &Self::AggregateID;
+  fn seq_nr(&self) -> usize;
+  fn occurred_at(&self) -> &DateTime<Utc>;
+  fn is_created(&self) -> bool;
+}
+
+/// 集約を表すトレイト。
+pub trait Aggregate: Debug + Send + Sync {
+  type ID: AggregateId;
+  /// IDを返す。
+  fn id(&self) -> &Self::ID;
+  /// シーケンス番号を返す。
+  fn seq_nr(&self) -> usize;
+  /// バージョンを返す。
+  fn version(&self) -> usize;
+  /// シーケンス番号を設定する。
+  fn set_version(&mut self, version: usize);
+}
+
+#[async_trait]
+pub trait EventPersistenceGateway: Debug + Clone + Sync + Send + 'static {
+  async fn get_snapshot_by_id<T, AID: AggregateId>(&self, aid: &AID) -> Result<(T, usize, usize)>
+  where
+    T: ?Sized + Serialize + Aggregate + for<'de> de::Deserialize<'de>;
+
+  async fn get_events_by_id_and_seq_nr<T, AID: AggregateId>(&self, aid: &AID, seq_nr: usize) -> Result<Vec<T>>
+  where
+    T: Debug + for<'de> de::Deserialize<'de>;
+
+  async fn store_event_with_snapshot_opt<A, E>(
+    &mut self,
+    event: &E,
+    version: usize,
+    aggregate: Option<&A>,
+  ) -> Result<()>
+  where
+    A: ?Sized + Serialize + Aggregate,
+    E: ?Sized + Serialize + Event;
+}
